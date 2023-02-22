9486192cf96600092ef886e2c1cd0798ac752d86
Marcin Zboralski <32996203+MarcinZboralski@users.noreply.github.com>
2023-02-22 08:20:39 +0100

    Dodaj pliki projektów.

---
+++ PolishTranslation/Localization.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using KSP.Localization;
+using System.Linq;
+using TMPro;
+
+namespace PolishTranslation
+{
+    [KSPAddon(KSPAddon.Startup.EveryScene,false)]
+    public class Localization : MonoBehaviour
+    {
+        public void Awake()
+        {
+            FontLoader fontLoader = FindObjectOfType<FontLoader>();
+            TMP_FontAsset fontAsset = fontLoader.LoadedFonts.FirstOrDefault(t => t.name.Equals("JD-LCD-PL-modified"));
+            fontLoader.AddMenuSubFont("pl", false, new[] { fontAsset });
+            fontLoader.SwitchFontLanguage("pl");
+            Localizer.SwitchToLanguage("pl");
+        }
+    }
+}
